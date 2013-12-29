ba5cf6e344a23d830edaa11ee6dd56fb570defcf
Joaquin Y Daniel <srdragoon@hotmail.com>
2013-12-29 10:36:44 +0000

    version 1.5

--- app/models/medida.rb
+++ app/models/medida.rb
@@ -1,6 +1,3 @@
 class Medida < ActiveRecord::Base
   belongs_to :Sensor
-  validates :nombre, :format => { :with => /\A[\w\.\-\_]+\z/ }
-  validates_presence_of :nombre
-  validates_uniqueness_of :nombre, :message => "El nombre debe ser Unico"
 end
